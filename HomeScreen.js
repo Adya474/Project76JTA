7421d50ab6c487f1cbbaa27bc9e78936c3b6bf4e
Adya474 <73647279+Adya474@users.noreply.github.com>
2021-07-22 13:29:27 +0530

    Add files via upload

---
+++ HomeScreen.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Text,StyleSheet,View} from 'react-native';
+
+export default class HomeScreen extends React.Component{
+
+    render(){
+        return(
+            <View style= {styles.container}>
+                <Text> Welcome to Jeff's Astrophysics Website! </Text>
+
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        justifyContent : "center",
+        alignItems: "center",
+        backgroundColor: 'black',
+        fontSize: 20,
+        color: 'white'
+    }
+
+})
